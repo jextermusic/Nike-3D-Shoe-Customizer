09fedb6b62927b8b85aaa572c68a6e7e4aead16d
jextermusic <djchiragthebest20@gmail.com>
2024-11-23 14:31:08 +0530

    deploy 2

--- src/Components/NewScene.jsx
+++ src/Components/NewScene.jsx
@@ -65,7 +65,7 @@ const NewScene = () => {
     
             SceneLoader.ImportMesh(
                 "",
-                "/src/assets/",
+                "/src/assets/models",
                 "jordanscompressed.glb",
                 scene,
                 function(meshes) {
